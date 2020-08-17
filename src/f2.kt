1bbdc4ed23bfbadc6d3cd2fae303abbbb5887062
Stiga290620 <arch20des@mail.rus>
2020-08-17 17:21:04 +0300

    v0 1720

---
+++ src/f2.kt
@@ -0,0 +1,20 @@
+import java.io.FileReader
+import java.util.*
+
+fun main() {
+    val sc=Scanner(FileReader("1234.txt"))
+    val a=sc.nextLine()
+    val b=sc.nextLine()
+
+    sc.close()
+
+
+    println(a)
+    println(a)
+
+    print(a)
+
+    print(b)
+
+    print(b)
+}
